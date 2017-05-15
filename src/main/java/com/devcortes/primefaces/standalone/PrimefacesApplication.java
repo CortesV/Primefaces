841ec15a554bf556d773fac33352220dc254b91d
CortesV <vitya.semenyuk1995@gmail.com>
2017-05-15 16:20:12 +0300

    CRUD on car entity

--- src/main/java/com/devcortes/primefaces/standalone/PrimefacesApplication.java
+++ src/main/java/com/devcortes/primefaces/standalone/PrimefacesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 @SpringBootApplication
+@ComponentScan({ "com.devcortes.primefaces" })
 public class PrimefacesApplication{
 
 	public static void main(String[] args) {
